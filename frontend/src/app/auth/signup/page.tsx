e2231315ac9c4b597817785fec4c25b9d687859c
debnath <debnath.sinhababu@elivaas.com>
2025-08-27 21:52:40 +0530

    build issue fixed

--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react'
 import Signup from './component/signup'
 
 export default function SignupPage() {
-  return <Signup />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Signup />
+    </Suspense>
+  )
 }
